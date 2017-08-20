c5b5ddc524d7e6e77852e12f642a336e0826e5fa
viik36 <vedantadas99@gmail.com>
2017-08-20 13:50:25 +0530

    Update DPLL.cpp

--- DPLL.cpp
+++ DPLL.cpp
@@ -11,19 +11,45 @@ int vars,no_of_clauses;
 //vector< vector<int> > clauses;
 map<int, bool> sol;
 
+bool sort_clause(const vector<int> a,const vector<int> b){
+        return (a.size()<b.size());
+    }
+
 using namespace std;
 
 
 
-bool dpll(vector< vector<int> > clauses){
+bool dpll(vector< vector<int> > clauses,map<int,bool> assigned){
+
+    if(clauses.size()==0){
+        sol = assigned;
+        return true;
+    }
+    for(vector< vector<int> > :: iterator i=clauses.begin();i!=clauses.end();i++){
+            if(i->empty()){
+               return false;
+        }
+    }
+
 
     vector< vector<int> > :: iterator i;
     vector<int>:: iterator j;
-    map<int, bool> assigned;
 
-    auto remove_unit_clause = [&](vector<int> i)->bool{
+    cout<<"clause"<<endl;
+    for(i=clauses.begin();i<clauses.end();i++){
+
+            for(j=i->begin();j!=i->end();j++){
+                cout<<*j<<" ";
+            }
+            cout<<endl;
+    }
+    cout<<endl;
+
+    /*auto remove_unit_clause = [&](vector<int> i)->bool{
         return(i.size()==1);
-    };
+    };*/
+
+
 
     auto remove_true_clause = [&](vector<int>i)->bool{
         if(i.size()==1){
@@ -32,32 +58,25 @@ bool dpll(vector< vector<int> > clauses){
         vector<int>:: iterator it;
         bool is_true = false;
         for(it=i.begin();it!=i.end();it++){
-            if(assigned.find(*it)!=assigned.end()){
+
+            if(assigned.find(abs(*it))!=assigned.end()){
+                //cout<<(assigned[abs(*it)]+1)%2<<" ";
                 if(*it<0){
-                    is_true = (-1)*assigned[*it];
+                    is_true = (assigned[abs(*it)]+1)%2;
                 }
                 else if(*it>0){
-                    is_true = assigned[*it];
+                    is_true = assigned[abs(*it)];
                 }
             }
         }
         return is_true;
     };
 
+
     auto remove_false_literal=[&](int num)->bool{
-        return(assigned.find(abs(num))==assigned.end()?false:(num<0?assigned[num]:(-1)*assigned[num]));
+        return((assigned.find(abs(num))==assigned.end())?false:(num<0?assigned[abs(num)]:((assigned[abs(num)]+1)%2)));
     };
     //auto remove_true_clause = [&]()
-
-
-    if(clauses.size()==0){
-        return true;
-    }
-    for(i=clauses.begin();i!=clauses.end();i++){
-            if(i->empty()){
-               return false;
-        }
-    }
 
     //unit propagation
 
@@ -74,30 +93,43 @@ bool dpll(vector< vector<int> > clauses){
                 sol[index] = true;
                 assigned[index] = true;
             }
+
         }
     }
 
-    //remove unit and true clause
-    auto iterator = remove_if(clauses.begin(),clauses.end(),remove_true_clause);
-    clauses.erase(iterator,clauses.end());
-
-    //remove false literals
     for(i=clauses.begin();i<clauses.end();i++){
         auto iterator = remove_if(i->begin(),i->end(),remove_false_literal);
         i->erase(iterator,i->end());
     }
+    //remove unit and true clause
+    auto iterator = remove_if(clauses.begin(),clauses.end(),remove_true_clause);
+    clauses.erase(iterator,clauses.end());
 
-    //remove true clauses merge with unit clause
-    //auto iterator = remove_if(clauses.begin(),clauses.end(),remove_true_clause);
-    //clauses.erase(iterator,clauses.end());
 
-    //print clauses
-    for(i=clauses.begin();i<clauses.end();i++){
-        for(j=i->begin();j!=i->end();j++){
-            cout<<*j<<" ";
-        }
-        cout<<endl;
+    sort(clauses.begin(),clauses.end(),sort_clause);
+    vector<vector<int> > clause_copy = clauses;
+    for(i=clause_copy.begin();i<clause_copy.end();i++){
+            for(j=i->begin();j!=i->end();j++){
+
+                    assigned[abs(*j)] = true;
+                    bool l = dpll(clause_copy,assigned);
+
+                    assigned[abs(*j)] = false;
+                    bool m = dpll(clause_copy,assigned);
+                    return (l || m);
+            }
+
     }
+
+
+    cout<<"map"<<endl;
+    for(map<int ,bool>::iterator p=assigned.begin();p!=assigned.end();p++){
+       cout<<p->first<<" "<<p->second;
+       cout<<endl;
+    }
+
+
+return true;
 
 
 }
@@ -107,6 +139,7 @@ int main()
     string line;
     string p,cnf;
     vector< vector<int> > clauses;
+    map<int, bool> assigned;
 
     while(getline(cin,line)){
         if(line[0]== 'p'){
@@ -131,7 +164,7 @@ int main()
         }
         clauses.push_back(c);
     }
-    dpll(clauses);
+    cout<<dpll(clauses,assigned);
     //cout<<clauses.size();
 
     return 0;
